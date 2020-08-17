16a2617bb68f250554bfbbcf7db90df41511e415
Theodore Ni <3806110+tjni@users.noreply.github.com>
2020-08-17 16:29:44 -0700

    Updating some dependencies.

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -2,17 +2,17 @@
 // workflow to cache downloaded dependencies between builds.
 
 val platforms = mapOf(
-    "dagger" to "2.25.3",
-    "immutables" to "2.8.2",
-    "mockito" to "3.2.4"
+    "dagger" to "2.28.3",
+    "immutables" to "2.8.8",
+    "mockito" to "3.3.3"
 )
 
 extra["dependencies"] = listOf(
     "com.google.dagger:dagger:${platforms["dagger"]}",
     "com.google.dagger:dagger-compiler:${platforms["dagger"]}",
     "javax.inject:javax.inject:1",
-    "org.junit.jupiter:junit-jupiter:5.6.0",
-    "one.util:streamex:0.7.2",
+    "org.junit.jupiter:junit-jupiter:5.6.2",
+    "one.util:streamex:0.7.3",
     "org.assertj:assertj-core:3.15.0",
     "org.freemarker:freemarker:2.3.29",
     "org.immutables:value:${platforms["immutables"]}",
